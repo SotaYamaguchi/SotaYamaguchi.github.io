b94b6a6fb10368142bdf74425ce1b25203e0c72e
Sota <area029_0709_sub@yahoo.co.jp>
2019-03-16 17:03:08 +0900

    build setting
    buildメソッド作成

--- config/index.js
+++ config/index.js
@@ -6,7 +6,6 @@ const path = require('path')
 
 module.exports = {
   dev: {
-
     // Paths
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
@@ -45,13 +44,16 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    // index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../docs/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    // assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '',
-    // assetsPublicPath: '/', ビルド時に相対パスで書き出す
+    assetsPublicPath: '', /* ビルド時に相対パスで書き出す */
+    // assetsPublicPath: '/',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
-import Output from '@/components/Output' //1. import
+import Output from '@/components/Output' // 1. import
 
 Vue.use(Router)
 
