08613d3f21f9e38b11c74809562cd0fbe4d9fa04
Sota <area029_0709_sub@yahoo.co.jp>
2019-03-24 04:34:37 +0900

    WetherChartAdd
    WetherChart機能実装

--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -10,17 +10,27 @@
       <div>
         <ol>
           <li v-for="(item, index) in items" :key=index>
-            <button @click="getIp(item.title)">
+            <button @click="getIp(item.title), getCanvas(item.title)">
               <p class="name">{{ item.title }}</p>
             </button>
           </li>
         </ol>
+      </div>
+      <div>
+        <div class="alert alert-info" v-show="loading">
+          Loading・・・
+        </div>
+        <div v-show="chart != null">
+          <canvas id="myChart"></canvas>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import Chart from 'chart.js'
+
 export default {
   name: 'Weather',
   data () {
@@ -34,9 +44,14 @@ export default {
         { title: 'Fukuoka' }
       ],
 
+      chart: null,
       city: 'ここに地名が表示されます',
+      dates: [],
+      temps: [],
       temp: 'ここに気温が表示されます',
-      condition: { 'main': 'ここに天気が表示されます' }
+      condition: { 'main': 'ここに天気が表示されます' },
+      loading: false,
+      errord: false
     }
   },
 
@@ -44,14 +59,110 @@ export default {
     getIp (name) {
       this.ip = '天気情報を取得しています'
       this.$axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${name},jp&units=metric&appid=57cb0a695c789061bf61a35dca406dd4`)
-        .then((response) => {
+        .then(response => {
           this.city = response.data.name
           this.temp = this.roundup(response.data.main.temp) + '℃'
           this.condition = response.data.weather[0]
         })
-        .catch((reason) => {
+        .catch(reason => {
           this.ip = '天気情報の取得に失敗しました'
         })
+    },
+
+    getCanvas (name) {
+      this.loading = true
+
+      if (this.chart != null) {
+        this.chart.destroy()
+      }
+
+      this.$axios.get('https://api.openweathermap.org/data/2.5/forecast', {
+        params: {
+          q: name,
+          units: 'metric',
+          appid: '57cb0a695c789061bf61a35dca406dd4'
+        }
+      })
+        .then(response => {
+          this.dates = response.data.list.map(list => {
+            return list.dt_txt
+          })
+          this.temps = response.data.list.map(list => {
+            return list.main.temp
+          })
+
+          var ctx = document.getElementById('myChart')
+          this.chart = new Chart(ctx, {
+            type: 'line',
+            data: {
+              labels: this.dates,
+              datasets: [
+                {
+                  label: 'Avg. Temp',
+                  backgroundColor: 'rgba(54, 162, 235, 0.5)',
+                  borderColor: 'rgb(54, 162, 235)',
+                  fill: false,
+                  data: this.temps
+                }
+              ]
+            },
+            options: {
+              title: {
+                display: true,
+                text: 'Temperature with Chart.js'
+              },
+              tooltips: {
+                callbacks: {
+                  label: function (tooltipItem, data) {
+                    var label = data.datasets[tooltipItem.dataseIndex].label || ''
+                    if (label) {
+                      label += ': '
+                    }
+
+                    label += Math.floor(tooltipItem.yLabel)
+                    return label + '℃'
+                  }
+                }
+              },
+              scales: {
+                xAxes: [
+                  {
+                    type: 'time',
+                    time: {
+                      unit: 'hour',
+                      displayFormats: {
+                        hour: 'M/DD @ hA'
+                      },
+                      tooltipFormat: 'MMM. DD @ hA'
+                    },
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Data/Time'
+                    }
+                  }
+                ],
+                yAxes: [
+                  {
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Tempereture (℃)'
+                    },
+                    ticks: {
+                      callback: function (value, index, values) {
+                        return value + '℃'
+                      }
+                    }
+                  }
+                ]
+              }
+            }
+          })
+        })
+        .catch(error => {
+          console.log(error)
+          this.errord = true
+        })
+        .finally(() => (this.loading = false))
     },
 
     roundup (value) {
