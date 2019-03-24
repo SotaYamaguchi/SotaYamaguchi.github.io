cb3932e882b87ed4837e234698303a6e901bc7c2
Sota <area029_0709_sub@yahoo.co.jp>
2019-03-24 19:25:05 +0900

    output_modified_2
    output修正2

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -2,8 +2,8 @@
     <div class="output">
         <div v-for="(item, index) in items" :key=index>
             <div class="content">
-                <h3> {{ item.title }} </h3>
-                <p>  {{ item.text }} </p>
+                <h3 v-html="$sanitize(item.title)"> {{ item.title }} </h3>
+                <p v-html="$sanitize(item.text)">  {{ item.text }} </p>
             </div>
         </div>
     </div>
@@ -18,8 +18,7 @@ export default {
         { title: 'お天気アプリを作成', text: 'Vue.jsでの開発に向けて勉強のため作成した。' },
         { title: 'VueCliを使用', text: 'パッケージマネージャーは参考にしたサンプルコードに従ってnpmを使用したが、¥nFaceBook等によって開発されたyarnの方が機能面で下記の2点で有利らしい¥n・タスクが並行して実行される為速度が早い¥n・npmはバージョンの不整合が起こる可能性がある' },
         { title: 'Chart.jsを使用', text: 'chart.jsのプロパティがわからなかったが勉強になった。' },
-        { title: 'axiosでJSONファイルを取得', text: 'OpenweatherMapのapi(weather)、(forecast)を使用し¥n選択した場所の現在の天気と3時間ごとの気象情報をグラフで表示する¥n※精度に問題あり？' },
-        { title: 'を使用', text: '五日間の¥n3時間ごとの予報で精度もいまいちなのであとで他のapiも試してみる。' }
+        { title: 'axiosでJSONファイルを取得', text: 'OpenweatherMapのapi(weather)、(forecast)を使用し¥n選択した場所の現在の天気と3時間ごとの気象情報をグラフで表示する¥n※精度に問題あり？' }
       ]
     }
   }
@@ -36,7 +35,7 @@ export default {
         background-color: #eee;
         display: inline-block;
         margin: 10px;
-        width: 30%;
+        width: 40%;
     }
 
     h3 {
