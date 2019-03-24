9d92a0739d3a46f22c4aa5f0b5ec16ac74d40cfa
Sota <area029_0709_sub@yahoo.co.jp>
2019-03-24 22:31:28 +0900

    output_text_change
    text修正

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -17,7 +17,7 @@ export default {
       items: [
         { title: 'お天気アプリを作成', text: 'Vue.jsでの開発に向けて勉強のため作成した。' },
         { title: 'VueCliを使用', text: 'パッケージマネージャーは参考にしたサンプルコードに従ってnpmを使用したが、FaceBook等によって開発されたyarnの方が機能面で下記の2点で有利らしい①タスクが並行して実行される為速度が早い②npmはバージョンの不整合が起こる可能性がある' },
-        { title: 'Chart.jsを使用', text: 'chart.jsのプロパティがわからなかったが勉強になった。' },
+        { title: 'Chart.jsを使用', text: ' webでグラフを実装したことがなかった為勉強になった。' },
         { title: 'axiosでJSONファイルを取得', text: 'OpenweatherMapのapi(weather)、(forecast)を使用し選択した場所の現在の天気と3時間ごとの気象情報をグラフで表示する※精度に問題あり？' }
       ]
     }
