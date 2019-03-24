29d6ddac76687d42d0c54c4b8ac021d1a2ce480b
Sota <area029_0709_sub@yahoo.co.jp>
2019-03-24 22:24:34 +0900

    install yarn
    npmのlockファイルでバージョンの差異が発生してビルドできないためyarnを導入

--- src/components/Output.vue
+++ src/components/Output.vue
@@ -15,11 +15,10 @@ export default {
   data () {
     return {
       items: [
-        { title: 'お天気アプリを作成した', text: 'このような意図で作成しました。' },
-        { title: 'VueCliを使用した', text: 'ここをこのように工夫しました。ここが難しかったです。今後はこうゆう機能を追加したいです' },
-        { title: 'Chart.jsを使用した', text: 'ここが難しかったです。' },
-        { title: 'OpenweatherMapのapi(weather)を使用した', text: '今後はこうゆう機能を追加したいです' },
-        { title: 'OpenweatherMapのapi(forecast)を使用した', text: 'このような意図で作成しました。ここをこのように工夫しました。ここが難しかったです。今後はこうゆう機能を追加したいです' }
+        { title: 'お天気アプリを作成', text: 'Vue.jsでの開発に向けて勉強のため作成した。' },
+        { title: 'VueCliを使用', text: 'パッケージマネージャーは参考にしたサンプルコードに従ってnpmを使用したが、FaceBook等によって開発されたyarnの方が機能面で下記の2点で有利らしい①タスクが並行して実行される為速度が早い②npmはバージョンの不整合が起こる可能性がある' },
+        { title: 'Chart.jsを使用', text: 'chart.jsのプロパティがわからなかったが勉強になった。' },
+        { title: 'axiosでJSONファイルを取得', text: 'OpenweatherMapのapi(weather)、(forecast)を使用し選択した場所の現在の天気と3時間ごとの気象情報をグラフで表示する※精度に問題あり？' }
       ]
     }
   }
@@ -36,7 +35,7 @@ export default {
         background-color: #eee;
         display: inline-block;
         margin: 10px;
-        width: 30%;
+        width: 45%;
     }
 
     h3 {
